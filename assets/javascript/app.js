0c8ba1e9d9ef04e234db7a839362c2ff7cb71b43
Tatum Daily <tgd9019@outlook.com>
2020-01-15 18:28:19 -0600

    Break Time

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,18 +6,20 @@
         
         var correct = 0;
         var wrong = 0;
+        // var questionOne = gameStarted();
+        // var questionTwo = gameQuestionTwo();
 
     // Arrays //
     // ========================================================== //
 
-
+    var gameQuestions = ["Who is the only President to serve more than eight years?", "Abraham Lincoln is part of Mt. Rushmore", "", "", ""];
+    var possibleAnswers = [["1. George Washington", "<br>", "2.Franklin Roosevelt", "<br>", "3.Andrew Jackson"], ["1. True", "<br>", "2. False", "<br>", ""], ["", "", ""], ["", "", ""], ["", "", ""]];
+    var rightAnswer = ["Franklin Roosevelt", "True", "", "", ""];
 
     // Objects //
     // ========================================================== //
 
-        var gameQuestions = ["Who is the only President to serve more than eight years?", "", "", "", ""];
-        var possibleAnswers = [["1. George Washington", "<br>", "2.Franklin Roosevelt", "<br>", "3.Andrew Jackson"], ["", "", ""], ["", "", ""], ["", "", ""], ["", "", ""]];
-        var rightAnswer = ["Franklin Roosevelt", "", "", "", ""];
+       
 
 // Functions //
 // ========================================================== //
@@ -32,7 +34,7 @@
             $(".startGameButtonDiv").on("click", gameStarted)
         }
 
-    // Game Started //
+    // Game Started / Question #1 //
     // ========================================================== //
 
         function gameStarted() {
@@ -40,17 +42,29 @@
             // $(".gameTimer")            
             $(".gameQuestions").html(gameQuestions[0])
             $(".startGameButtonDiv").html("")
-            $(".possibleOne").html(possibleAnswers[0.0])
-            $(".possibleTwo").html(possibleAnswers[0.1])
-            $(".possibleThree").html(possibleAnswers[0.2])
+            $(".possibleOne").prop('value', possibleAnswers[0.0]);
+            $(".possibleTwo").val(possibleAnswers[0.1])
+            $(".possibleThree").val(possibleAnswers[0.2])
+
+            
         }
 
-    // Answer Clicks //
+    // Question Two //
     // ========================================================== //
     
+        function gameQuestionTwo() {
+            $(".gameTitle").html("Presidential Memory")
+            // $(".gameTimer")            
+            $(".gameQuestions").html(gameQuestions[1])
+            $(".startGameButtonDiv").html("")
+            $(".possibleOne").html(possibleAnswers[1.0])
+            $(".possibleTwo").html(possibleAnswers[1.1])
+            $(".possibleThree").html(possibleAnswers[1.2])
+        }
         
 
 // Main Process //
 // ========================================================== //
 
     pageLoad()
+    // $("possibleTwo").on("click", gameQuestionTwo)
